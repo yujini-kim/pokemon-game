246ca97e90962c20a848d6ed94ab83769fb3d1bf
yujin <kingyujin56@gmail.com>
2024-09-03 02:04:39 +0900

    classname

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html"],
+  content: ["./src/**/*.html",
+    "./src/css/**/*.css"
+  ],
   theme: {
     extend: {
       screens: {
